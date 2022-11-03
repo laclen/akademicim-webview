2f392f29b68daec7e645603f7589d1db3fc43046
Celal Zamanoglu <celal.zamanoglu@hotmail.com>
2022-11-03 20:56:01 +0300

    first commmit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+
+// navigation
+const NavigationView = () => {
+  <View style={styles.navigationContainer}>
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.buttonTitle}>Back</Text>
+    </TouchableOpacity>
+
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.buttonTitle}>Forward</Text>
+    </TouchableOpacity>
+
+  </View>
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView
+      allowNackForwardNavigationGestures={true}
+      style={styles.container}
+      source={{ uri: 'https://akademicim.com' }}
+      />
+      <NavigationView/>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  navigationContainer: { 
+    height: 60,
+    backgroundColor: "#1B1B58",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  button: {
+
+
+  },
+  buttonTitle:{},
 });
