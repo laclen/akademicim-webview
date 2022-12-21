a8fd008aadc8431e8475f0bfded8d02715c279c8
Celal Zamanoğlu <celalzamanoglu@Celals-MacBook-Pro.local>
2022-12-21 15:49:48 +0300

    last edits

--- App.js
+++ App.js
@@ -1,64 +1,61 @@
-import * as React from "react";
-import { WebView } from "react-native-webview";
-import { StyleSheet, Platform, BackHandler } from "react-native";
+import { useEffect, useRef } from "react"
+import { WebView } from "react-native-webview"
+import { StyleSheet, Platform, BackHandler } from "react-native"
 
 export default function App() {
-  
+  const webViewRef = useRef()
+
   // navigate back function for android
-  const webViewRef = React.useRef();
   const onAndroidBackpress = () => {
     if (webViewRef.current) {
-      webViewRef.current.goBack();
-      return true;
+      webViewRef.current.goBack()
+      return true
     }
-    return false;
-  };
+    return false
+  }
 
-  // WebView does not support swipe back for android 
+  // WebView does not support swipe back for some android devices
   // thus we have to handle the goBack function with hardwareBackPress
-  React.useEffect(() => {
+  useEffect(() => {
     if (Platform.OS === "android") {
-      BackHandler.addEventListener("hardwareBackPress", onAndroidBackpress);
+      BackHandler.addEventListener("hardwareBackPress", onAndroidBackpress)
       return () => {
-        BackHandler.removeEventListener(
-          "hardwareBackPress",
-          onAndroidBackpress
-        );
-      };
+        BackHandler.removeEventListener("hardwareBackPress", onAndroidBackpress)
+      }
     }
-  }, []);
+  }, [])
 
   return (
     <WebView
-      // handle the goBack function with swipe for android
-      onTouchStart={e =>  {
-        if(Platform.OS === 'android'){ 
-          this.touchX = e.nativeEvent.pageX; 
-          this.touchY = e.nativeEvent.pageY 
+      // handle the goBack function with swipe gesture for some android devices
+      onTouchStart={(e) => {
+        if (Platform.OS === "android") {
+          this.touchX = e.nativeEvent.pageX
+          this.touchY = e.nativeEvent.pageY
         } else {
           return null
-        }}
-      }
-      onTouchEnd={e =>  {
-        if(Platform.OS === 'android' && this.touchX - e.nativeEvent.pageX < -20){
-          if(this.touchY - e.nativeEvent.pageY > -20 && this.touchY - e.nativeEvent.pageY < 20){
+        }
+      }}
+      onTouchEnd={(e) => {
+        if (Platform.OS === "android" && this.touchX - e.nativeEvent.pageX < -20) {
+          if (this.touchY - e.nativeEvent.pageY > -20 && this.touchY - e.nativeEvent.pageY < 20) {
             onAndroidBackpress()
           }
         } else {
           return null
-        }}
-      }
+        }
+      }}
       allowsBackForwardNavigationGestures // only works with iOS
       allowsInlineMediaPlayback
       ref={webViewRef}
       style={styles.container}
       source={{ uri: "https://akademicim.com" }}
     />
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
-});
+    flex: 1,
+  },
+})
